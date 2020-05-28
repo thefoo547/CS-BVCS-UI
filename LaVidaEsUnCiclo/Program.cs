ff32ab90b65c626ee3a8000b1fbae24abdc3dc86
thefoo@win <jf_mejiar547@live.com>
2020-05-28 14:58:08 -0600

    Ejemplos Ciclos

---
+++ LaVidaEsUnCiclo/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace LaVidaEsUnCiclo
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            //int i = 25;
+            /*i = i + 5;
+            i += 5;*/
+            /*i = i -5;
+            i -= 5;*/
+            /*i = i *5;
+            i *= 5;*/
+            /*
+            i = i/5;
+            i /= 5;
+            */
+            
+            int r = 0;
+            /*
+            for (int i = 1; i <= 10 && r == 'y' ; i++)
+            {
+                Console.WriteLine(i);
+                Console.WriteLine("Quieres que siga el bucle? (Y/N)");
+                r = Convert.ToChar(Console.Read());
+            }
+            */
+            /*
+            while (r == 'y')
+            {
+                Console.WriteLine("Quieres que siga el bucle? (Y/N)");
+                
+                r = Convert.ToChar(Console.ReadLine());
+            }
+            */
+            /*
+            do
+            {
+                Console.WriteLine("Ingrese la medida: ");
+
+                r = Convert.ToInt32(Console.ReadLine());
+            } while (r < 0);
+            Console.WriteLine("Hello World!");
+            */
+        }
+    }
+}
