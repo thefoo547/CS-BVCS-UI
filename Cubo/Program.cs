e9978be66ceef4f1d235da96d60fc8351fa61d8e
thefoo@win <jf_mejiar547@live.com>
2020-05-28 16:54:23 -0600

    Agregado cubo y string

---
+++ Cubo/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+/*  Crear un programa que imprima un cubo de n * n
+ * con la condicion que n no puede ser menor que 0
+ * y mayor que 23
+ * 
+ * 
+ */
+
+
+namespace Cubo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 0;
+            // Do while para validar
+            do {
+                Console.Write("Ingrese n: ");
+                n = Convert.ToInt32(Console.ReadLine());
+                // que n no sea mayor que 25 y menor que 0
+            } while (n < 0 || n > 25);
+            // for i imprime las columnas
+            for (int i = 0; i < n; i++)
+            {
+                // for j imprime las filas
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write("#");
+                }
+                // salto de linea despues de cada fila
+                Console.WriteLine();
+            }
+            /*
+             * ###
+             * ###
+             * ###
+             */
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Strings/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre = Console.ReadLine();
+            Console.WriteLine("Hello "+nombre);
+        }
+    }
+}
