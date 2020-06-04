7c620b05fa57b8feec6efdcb67d8077f93cb530e
atlasfoo@win <jf_mejiar547@live.com>
2020-06-04 17:54:39 -0600

    agregados dos ejercicios

---
+++ Figuras/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace Figuras
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool fl;
+            float area = 0;
+            do
+            {
+                Console.WriteLine("Seleccione una figura geométrica");
+
+                Console.WriteLine("1. Rectángulo");
+                Console.WriteLine("2. Circulo");
+                Console.WriteLine("3. Triángulo");
+
+                int opt = Convert.ToInt32(Console.ReadLine());
+                
+                fl = false;
+                switch (opt)
+                {
+                    case 1:
+                        area = area_rectángulo(2, 5);
+                        break;
+                    case 2:
+                        area = area_circulo(5);
+                        break;
+                    case 3:
+                        triangulo();
+                        break;
+                    default:
+                        Console.WriteLine("Opción no válida");
+                        fl = true;
+                        break;
+                }
+                Console.ReadKey();
+                Console.Clear();
+            } while (fl);
+        }
+        static void triangulo()
+        {
+            float b, h;
+            do
+            { 
+                Console.Write("Ingrese la base: ");
+                b = Convert.ToSingle(Console.ReadLine());
+                Console.Write("Ingrese la altura: ");
+                h = Convert.ToSingle(Console.ReadLine());
+                Console.Clear();
+            } while (b < 0 || h < 0);
+
+            Console.WriteLine($"El área del triángulo es {area_triangulo(b, h)}");
+        }
+
+        static float area_rectángulo(float b, float h)
+        {
+            return b * h;
+        }
+        static float area_circulo(float r)
+        {
+            return (float)((float)Math.PI * Math.Pow(r,2));
+        }
+        static float area_triangulo(float b, float h)
+        {
+            return (b * h) / 2;
+        }
+    }
+}
---
+++ GreedyCS/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace GreedyCS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float dinero;
+            do
+            {
+                Console.Write("Ingrese la cantidad de dinero: ");
+                dinero = Convert.ToSingle(Console.ReadLine());
+            } while (dinero < 0);
+            int centavos = (int)Math.Round(dinero * 100);
+
+            int mon_25 = centavos / 25;
+            centavos %= 25;
+            int mon_10 = centavos / 10;
+            centavos %= 10;
+            int mon_5 = centavos / 5;
+            centavos %= 5;
+            int mon_1 = centavos / 1;
+
+            Console.WriteLine($"Se extraen {mon_25} monedas de 25, {mon_10} monedas de 10, {mon_5} monedas de 5 y {mon_1} monedas de 1 de {dinero:F2}$");
+        }
+    }
+}
