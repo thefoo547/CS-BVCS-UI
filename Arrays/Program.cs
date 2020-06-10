10c7168b88681a4587bb4a2bee4dc73a08e107a6
atlasfoo@win <jf_mejiar547@live.com>
2020-06-10 11:03:52 -0600

    Arreglos

---
+++ Arrays/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Specialized;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Cuantos numeros va a ingresar?");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int[] vct = { 45, 75, 223 };
+            int[] arr = new int[3];
+
+
+            leer_arreglo(arr);
+
+            imprimir_arreglo(arr);
+            
+        }
+
+        static void leer_arreglo(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write($"Numero {i + 1}:");
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+
+
+        static void imprimir_arreglo(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+        }
+    }
+}
---
+++ Matrix/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Matrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Cantidad de filas");
+            int m = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Cantidad de columnas");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            // si el arreglo no tenía valores
+            int[,] matriz = new int[m, n];
+            //i para las filas
+            for (int i = 0; i < m; i++)
+            {
+                //j para las columnas
+                for (int j = 0; j < n; j++)
+                {
+                    matriz[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+            imprimir_matriz(matriz);
+
+            int[,] mt = { { 5, 6, 8 }, { 74, 99, 156 } };
+            imprimir_matriz(mt);
+        }
+
+        static void imprimir_matriz(int[,] mtx)
+        {
+            Console.WriteLine("----------------");
+            for (int i = 0; i < mtx.GetLength(1); i++)
+            {
+                for (int j = 0; j < mtx.GetLength(0); j++)
+                {
+                    Console.Write($"|{mtx[j, i]}| ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
